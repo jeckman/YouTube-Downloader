c98e66a607344c5199a5e0ff6636f09702b01ad1
Sepehr Lajevardi <me@sepehr.ws>
2013-05-27 04:15:20 +0430

    Stupid simple proxy download option.

---
+++ download.php
@@ -0,0 +1,42 @@
+<?php
+
+// Check download token
+if (empty($_GET['mime']) OR empty($_GET['token']))
+{
+	exit('Invalid download token 8{');
+}
+
+// Set operation params
+$mime = filter_var($_GET['mime']);
+$ext  = str_replace(array('/', 'x-'), '', strstr($mime, '/'));
+$url  = base64_decode(filter_var($_GET['token']));
+$name = "videoplayback.$ext"; // @TODO: Get the actual title
+
+// Fetch and serve
+if ($url)
+{
+	// Generate the server headers
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
+	{
+		header('Content-Type: "' . $mime . '"');
+		header('Content-Disposition: attachment; filename="' . $name . '"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header("Content-Transfer-Encoding: binary");
+		header('Pragma: public');
+	}
+	else
+	{
+		header('Content-Type: "' . $mime . '"');
+		header('Content-Disposition: attachment; filename="' . $name . '"');
+		header("Content-Transfer-Encoding: binary");
+		header('Expires: 0');
+		header('Pragma: no-cache');
+	}
+
+	readfile($url);
+	exit;
+}
+
+// Not found
+exit('File not found 8{');
--- getvideo.php
+++ getvideo.php
@@ -3,16 +3,16 @@
 // based on youtube-dl in Python http://rg3.github.com/youtube-dl/
 // by Ricardo Garcia Gonzalez and others (details at url above)
 //
-// Takes a VideoID and outputs a list of formats in which the video can be 
-// downloaded 
+// Takes a VideoID and outputs a list of formats in which the video can be
+// downloaded
 
 include_once('curl.php');
 
 if(isset($_REQUEST['videoid'])) {
-	$my_id = $_REQUEST['videoid']; 
-} else {	
+	$my_id = $_REQUEST['videoid'];
+} else {
 	echo '<p>No video id passed in</p>';
-	exit; 
+	exit;
 }
 
 if(isset($_REQUEST['type'])) {
@@ -24,7 +24,7 @@ if(isset($_REQUEST['type'])) {
 if(isset($_REQUEST['debug'])) {
 	$debug = TRUE;
 } else {
-	$debug = FALSE; 	
+	$debug = FALSE;
 }
 
 if ($my_type == 'Download') {
@@ -42,7 +42,7 @@ if ($my_type == 'Download') {
         padding-bottom: 40px;
         background-color: #f5f5f5;
 	}
-    
+
 	  .download {
         max-width: 300px;
         padding: 19px 29px 29px;
@@ -60,26 +60,35 @@ if ($my_type == 'Download') {
       .download .download-heading {
         margin-bottom: 10px;
       }
+
+      .mime, .itag {
+      	width: 75px;
+		display: inline-block;
+      }
+
+      .itag {
+      	width: 15px;
+      }
     </style>
 	</head>
-<body>	
+<body>
 	<div class="download">
 	<h1 class="download-heading">Youtube Downloader Results</h1>
-<?php 
+<?php
 } // end of if for type=Download
 
-/* First get the video info page for this video id */ 
+/* First get the video info page for this video id */
 $my_video_info = 'http://www.youtube.com/get_video_info?&video_id='. $my_id;
 $my_video_info = curlGet($my_video_info);
 
-/* TODO: Check return from curl for status code */ 
+/* TODO: Check return from curl for status code */
 
-parse_str($my_video_info); 
+parse_str($my_video_info);
 echo '<p><img src="'. $thumbnail_url .'" border="0" hspace="2" vspace="2"></p>';
 
 if(isset($url_encoded_fmt_stream_map)) {
-	/* Now get the url_encoded_fmt_stream_map, and explode on comma */ 
-	$my_formats_array = explode(',',$url_encoded_fmt_stream_map); 
+	/* Now get the url_encoded_fmt_stream_map, and explode on comma */
+	$my_formats_array = explode(',',$url_encoded_fmt_stream_map);
 	//if($debug) {
 	//	echo '<pre>';
 	//	print_r($my_formats_array);
@@ -88,29 +97,29 @@ if(isset($url_encoded_fmt_stream_map)) {
 } else {
 	echo '<p>No encoded format stream found.</p>';
 	echo '<p>Here is what we got from YouTube:</p>';
-	echo $my_video_info; 
+	echo $my_video_info;
 }
 if (count($my_formats_array) == 0) {
 	echo '<p>No format stream map found - was the video id correct?</p>';
 	exit;
 }
 
-/* create an array of available download formats */ 
+/* create an array of available download formats */
 $avail_formats[] = '';
-$i = 0; 
+$i = 0;
 
 foreach($my_formats_array as $format) {
-	parse_str($format); 
-	$avail_formats[$i]['itag'] = $itag; 
-	$avail_formats[$i]['quality'] = $quality; 
-	$type = explode(';',$type); 
+	parse_str($format);
+	$avail_formats[$i]['itag'] = $itag;
+	$avail_formats[$i]['quality'] = $quality;
+	$type = explode(';',$type);
 	$avail_formats[$i]['type'] = $type[0];
-	$avail_formats[$i]['url'] = urldecode($url) . '&signature=' . $sig; 
+	$avail_formats[$i]['url'] = urldecode($url) . '&signature=' . $sig;
 	parse_str(urldecode($url));
 	$avail_formats[$i]['expires'] = date("G:i:s T", $expire);
 	$avail_formats[$i]['ipbits'] = $ipbits;
 	$avail_formats[$i]['ip'] = $ip;
-	$i++; 
+	$i++;
 }
 
 if ($debug) {
@@ -119,66 +128,75 @@ if ($debug) {
 	echo 'Note that when 8 bit IP addresses are used, the download links may fail.</p>';
 }
 if ($my_type == 'Download') {
-	echo '<ul>List of Available Formats for Download - right-click and choose "save as"</ul>'; 
-	/* now that we have the array, print the options */ 
-	for ($i = 0; $i< count($avail_formats); $i++) {
-		echo '<li>' . $avail_formats[$i]['itag'] .' <a href="'. $avail_formats[$i]['url'] .'">'. $avail_formats[$i]['type'] .'</a> ('. $avail_formats[$i]['quality']  .')</li>';
+	echo '<ul>
+			List of available formats for download:<br>
+			<small>Right-click and choose "save as" or click "download" to use this server as proxy.</small>
+		</ul>';
+
+	/* now that we have the array, print the options */
+	for ($i = 0; $i < count($avail_formats); $i++) {
+		echo '<li>' .
+				'<span class="itag">' . $avail_formats[$i]['itag'] . '</span> '.
+				'<a href="' . $avail_formats[$i]['url'] . '" class="mime">' . $avail_formats[$i]['type'] . '</a> ' .
+				'<small>(' .  $avail_formats[$i]['quality'] . ' / ' .
+				'<a href="download.php?mime=' . $avail_formats[$i]['type'] . '&token=' . base64_encode($avail_formats[$i]['url']) . '" class="dl">download</a>' .
+				')</small></li>';
 	}
 	echo '</ul>';
 ?>
 </form>
 </body>
-</html>	
+</html>
 
-<?php 
+<?php
 
 } else {
 
 /* In this else, the request didn't come from a form but from something else
- * like an RSS feed. 
- * As a result, we just want to return the best format, which depends on what 
- * the user provided in the url. 
- * If they provided "format=best" we just use the largest. 
+ * like an RSS feed.
+ * As a result, we just want to return the best format, which depends on what
+ * the user provided in the url.
+ * If they provided "format=best" we just use the largest.
  * If they provided "format=free" we provide the best non-flash version
  * If they provided "format=ipad" we pull the best MP4 version
- * 
+ *
  * Thanks to the python based youtube-dl for info on the formats
  *   							http://rg3.github.com/youtube-dl/
- */  
+ */
 
 $format =  $_REQUEST['format'];
-$target_formats = ''; 
+$target_formats = '';
 switch ($format) {
 	case "best":
-		/* largest formats first */ 
+		/* largest formats first */
 		$target_formats = array('38', '37', '46', '22', '45', '35', '44', '34', '18', '43', '6', '5', '17', '13');
 		break;
 	case "free":
-		/* Here we include WebM but prefer it over FLV */ 
+		/* Here we include WebM but prefer it over FLV */
 		$target_formats = array('38', '46', '37', '45', '22', '44', '35', '43', '34', '18', '6', '5', '17', '13');
 		break;
 	case "ipad":
-		/* here we leave out WebM video and FLV - looking for MP4 */ 
-		$target_formats = array('37','22','18','17'); 
+		/* here we leave out WebM video and FLV - looking for MP4 */
+		$target_formats = array('37','22','18','17');
 		break;
-	default: 
+	default:
 		/* If they passed in a number use it */
 		if (is_numeric($format)) {
-			$target_formats[] = $format; 
+			$target_formats[] = $format;
 		} else {
 			$target_formats = array('38', '37', '46', '22', '45', '35', '44', '34', '18', '43', '6', '5', '17', '13');
 		}
 	break;
 }
 
-/* Now we need to find our best format in the list of available formats */ 
-$best_format = ''; 
+/* Now we need to find our best format in the list of available formats */
+$best_format = '';
 for ($i=0; $i < count($target_formats); $i++) {
 	for ($j=0; $j < count ($avail_formats); $j++) {
 		if($target_formats[$i] == $avail_formats[$j]['itag']) {
 			//echo '<p>Target format found, it is '. $avail_formats[$j]['itag'] .'</p>';
-			$best_format = $j; 
-			break 2; 
+			$best_format = $j;
+			break 2;
 		}
 	}
 }
@@ -186,6 +204,6 @@ for ($i=0; $i < count($target_formats); $i++) {
 //echo '<p>Out of loop, best_format is '. $best_format .'</p>';
 $redirect_url = $avail_formats[$best_format]['url'];
 $content_type = $avail_formats[$best_format]['type'];
-header("Location: $redirect_url"); 
+header("Location: $redirect_url");
 } // end of else for type not being Download
 ?>
