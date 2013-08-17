2880af5876cc28f600be5a9b85082ea7fb9ac6c5
Aires Gonçalves <airesvsg@gmail.com>
2013-08-17 19:32:51 -0300

    Now accepts url

--- getvideo.php
+++ getvideo.php
@@ -10,6 +10,29 @@ include_once('curl.php');
 
 if(isset($_REQUEST['videoid'])) {
 	$my_id = $_REQUEST['videoid'];
+	if(strlen($my_id)>11){
+		$url   = parse_url($my_id);
+		$my_id = NULL;
+		if( is_array($url) && count($url)>0 && isset($url['query']) && !empty($url['query']) ){
+			$parts = explode('&',$url['query']);
+			if( is_array($parts) && count($parts) > 0 ){
+				foreach( $parts as $p ){
+					$pattern = '/^v\=/';
+					if( preg_match($pattern, $p) ){
+						$my_id = preg_replace($pattern,'',$p);
+						break;
+					}
+				}
+			}
+			if( !$my_id ){
+				echo '<p>No video id passed in</p>';
+				exit;
+			}
+		}else{
+			echo '<p>Invalid url</p>';
+			exit;
+		}
+	}
 } else {
 	echo '<p>No video id passed in</p>';
 	exit;
