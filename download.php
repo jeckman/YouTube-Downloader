c5a060617c1277f5175f17c672d362e5d1db248b
BluesEagles <blues_eagles_live@hotmail.com>
2016-12-10 16:44:43 +0700

    Revert "File size isn't unknown for download managers now"

--- download.php
+++ download.php
@@ -1,6 +1,5 @@
 <?php
 
-include_once('config.php');
 // Check download token
 if (empty($_GET['mime']) OR empty($_GET['token']))
 {
@@ -16,14 +15,12 @@ $name = urldecode($_GET['title']). '.' .$ext;
 // Fetch and serve
 if ($url)
 {
-	$size=get_size($url);
 	// Generate the server headers
 	if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
 	{
 		header('Content-Type: "' . $mime . '"');
 		header('Content-Disposition: attachment; filename="' . $name . '"');
 		header('Expires: 0');
-		header('Content-Length: '.$size);
 		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 		header("Content-Transfer-Encoding: binary");
 		header('Pragma: public');
@@ -34,7 +31,6 @@ if ($url)
 		header('Content-Disposition: attachment; filename="' . $name . '"');
 		header("Content-Transfer-Encoding: binary");
 		header('Expires: 0');
-		header('Content-Length: '.$size);
 		header('Pragma: no-cache');
 	}
 
