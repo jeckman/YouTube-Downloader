e417aebcbcbd16a2d7bf3d86ab63734e5b8de235
Art4 <art4@wlabs.de>
2017-05-24 21:52:09 +0200

    Create unittest with PHPUnit

---
+++ tests/YoutubeDownloaderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace YoutubeDownloader\Tests;
+
+use YoutubeDownloader\YoutubeDownloader;
+
+class YoutubeDownloaderTest extends \PHPUnit\Framework\TestCase
+{
+	/**
+	 * @test validateVideoId()
+	 * @dataProvider VideoIdProvider
+	 */
+	public function validateVideoId($str, $expected)
+	{
+		$this->assertSame($expected, YoutubeDownloader::validateVideoId($str));
+	}
+
+	/**
+	 * dataprovider for validateVideoId()
+	 */
+	public function VideoIdProvider()
+	{
+		$id = 'dQw4w9WgXcQ';
+
+		return [
+			['http://youtu.be/dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/embed/dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/watch?v=dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/?v=dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/v/dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/e/dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/user/username#p/u/11/dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/sandalsResorts#p/c/54B8C800269D7C1B/0/dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/watch?feature=player_embedded&v=dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/?feature=player_embedded&v=dQw4w9WgXcQ', $id],
+			['http://www.youtube.com/v/dQw4w9WgXcQ?fs=1&hl=en_US', $id],
+			['https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ', $id],
+			['<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>', $id],
+		];
+	}
+}
