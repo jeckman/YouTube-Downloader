032705dcc848166cfb178e83bb7ac513ea607987
Art4 <art4@wlabs.de>
2017-07-17 13:29:45 +0200

    Create simple template engine with tests

---
+++ src/Template/Engine.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace YoutubeDownloader\Template;
+
+/**
+ * Template Engine
+ */
+class Engine
+{
+	/**
+	 * Creates the engine with a template directory
+	 *
+	 * @param VideoInfo $video_info
+	 * @return StreamMap
+	 */
+	public static function createFromDirectory($directory)
+	{
+		$directory = rtrim($directory, DIRECTORY_SEPARATOR);
+
+		return new self($directory);
+	}
+
+	/**
+	 * template directory.
+	 *
+	 * @var string
+	 */
+	private $directory;
+
+	/**
+	 * Creates the engine
+	 *
+	 * @param string $directory
+	 * @return self
+	 */
+	private function __construct($directory)
+	{
+		$this->directory = $directory;
+	}
+
+	/**
+	 * Get the template directory
+	 *
+	 * @return string
+	 */
+	public function getTemplateDirectory()
+	{
+		return $this->directory;
+	}
+
+	/**
+	 * Create a new template and render it
+	 *
+	 * @param  string $name
+	 * @param  array  $data
+	 * @return string
+	 */
+	public function render($name, array $data = array())
+	{
+		return $this->createTemplate($name)->render($data);
+	}
+
+	/**
+	 * Create a new template
+	 *
+	 * @param  string   $name
+	 * @return Template
+	 */
+	private function createTemplate($name)
+	{
+		return new Template($this, $name);
+	}
+}
---
+++ tests/Unit/Template/TemplateTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace YoutubeDownloader\Tests\Unit;
+
+use org\bovigo\vfs\vfsStream;
+use YoutubeDownloader\Template\Engine;
+use YoutubeDownloader\Template\Template;
+use YoutubeDownloader\Tests\Fixture\TestCase;
+
+class TemplateTest extends TestCase
+{
+	public function setUp()
+	{
+		vfsStream::setup('templates');
+	}
+
+	/**
+	 * @test __construct()
+	 */
+	public function construct()
+	{
+		$engine = $this->createMock(Engine::class);
+
+		$this->assertInstanceOf('YoutubeDownloader\Template\Template', new Template($engine, 'template.php'));
+	}
+
+	/**
+	 * @test get()
+	 * @dataProvider GetDataProvider
+	 */
+	public function get($data, $key, $default, $expected)
+	{
+		$engine = $this->createMock(Engine::class);
+
+		$template = new Template($engine, 'template.php');
+		$template->setData($data);
+
+		$this->assertSame($expected, $template->get($key, $default));
+	}
+
+	/**
+	 * GetDataProvider
+	 */
+	public function GetDataProvider()
+	{
+		return [
+			[['foo' => 'bar'], 'foo', null, 'bar'],
+			[['fuz' => 'baz'], 'foo', 'bar', 'bar'],
+			[[], 'foo', null, null],
+			[[], 'foo', 'bar', 'bar'],
+		];
+	}
+
+	/**
+	* @test render()
+	* @dataProvider RenderDataProvider
+	*/
+	public function render($filename, $file_content, $data, $expected)
+	{
+		$engine = $this->createMock(Engine::class);
+		$engine->method('getTemplateDirectory')
+			->willReturn(vfsStream::url('templates'));
+
+		vfsStream::create([$filename => $file_content]);
+
+		$template = new Template($engine, $filename);
+
+		$this->assertSame($expected, $template->render($data));
+	}
+
+	/**
+	* RenderDataProvider
+	*/
+	public function RenderDataProvider()
+	{
+		return [
+			['template1.php', '<html></html>', [], '<html></html>'],
+			[
+				'template2.php',
+				'<html><?php echo $this->get(\'foo\'); ?></html>',
+				['foo' => 'bar'],
+				'<html>bar</html>'
+			],
+			[
+				'template3.php',
+				'<html><?php echo $this->get(\'foo\', \'default\'); ?></html>',
+				['fuz' => 'baz'],
+				'<html>default</html>'
+			],
+		];
+	}
+
+	/**
+	* @test render()
+	*/
+	public function renderCannotFindFile()
+	{
+		$engine = $this->createMock(Engine::class);
+		$engine->method('getTemplateDirectory')
+			->willReturn(vfsStream::url('templates'));
+
+		// template.php don't exist
+		//vfsStream::create(['template.php' => '']);
+
+		$this->expectException('LogicException');
+		$this->expectExceptionMessage('The template "template.php" could not be found at "vfs://templates".');
+
+		$template = new Template($engine, 'template.php');
+
+		$template->render();
+	}
+
+	/**
+	* @test render()
+	*/
+	public function renderRethrowExceptionFromFile()
+	{
+		$engine = $this->createMock(Engine::class);
+		$engine->method('getTemplateDirectory')
+			->willReturn(vfsStream::url('templates'));
+
+		vfsStream::create([
+			'template.php' => '<?php throw new \Exception(\'exception message\'); ?>',
+		]);
+
+		$this->expectException('Exception');
+		$this->expectExceptionMessage('exception message');
+
+		$template = new Template($engine, 'template.php');
+
+		$template->render();
+	}
+}
