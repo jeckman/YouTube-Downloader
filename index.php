3a6e49122d7913b8f5687617bfa830666d868567
Artur Weigandt <Art4@users.noreply.github.com>
2017-07-18 10:33:21 +0200

    Remove Engine creating from index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 include_once('common.php');
 
-$template = \YoutubeDownloader\Template\Engine::createFromDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'templates');
-
 echo $template->render('index.php', [
 	'is_chrome' => \YoutubeDownloader\YoutubeDownloader::is_chrome(),
 	'showBrowserExtensions' => $config->get('showBrowserExtensions'),
