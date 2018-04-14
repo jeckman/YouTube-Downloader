615a71cd99de7cce8fe000e8629f3163baaa1a92
Bryce <antiteal@gmail.com>
2018-04-13 22:15:11 -0700

    Add alternative video info url
    Request an alternative video info URL if the normal one comes back with an error.

--- src/Provider/Youtube/Provider.php
+++ src/Provider/Youtube/Provider.php
@@ -133,6 +133,7 @@ final class Provider implements ProviderInterface, CacheAware, HttpClientAware, 
         /* First get the video info page for this video id */
         // $my_video_info = 'http://www.youtube.com/get_video_info?&video_id='. $input;
         // thanks to amit kumar @ bloggertale.com for sharing the fix
+      
         $video_info_url = 'http://www.youtube.com/get_video_info?&video_id=' . $input . '&asv=3&el=detailpage&hl=en_US';
 
         $request = $this->getHttpClient()->createRequest(
@@ -147,6 +148,16 @@ final class Provider implements ProviderInterface, CacheAware, HttpClientAware, 
         }
 
         $response = $this->getHttpClient()->send($request, $options);
+
+        /* Try alternative URL if the status is fail */
+        if (strpos($response->getBodyAsString(), 'status=fail') !== false) {
+            $video_info_url = 'http://www.youtube.com/get_video_info?&video_id=' . $input . '&asv=3&hl=en_US';
+            $request = $this->getHttpClient()->createRequest(
+                'GET',
+                $video_info_url
+            );
+            $response = $this->getHttpClient()->send($request, $options);
+        }
 
         /* TODO: Check response for status code and Content-Type */
         $video_info = VideoInfo::createFromStringWithOptions(
