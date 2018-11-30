382bdc203d8eec9eee6f5531cd22f950af0b7c2f
Art4 <art4@wlabs.de>
2018-11-30 14:51:46 +0100

    require composer autoloader, YoutubeDownloader\Logger implements Psr\Log

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -20,6 +20,8 @@
 
 namespace YoutubeDownloader\Logger;
 
+use Psr\Log\LoggerInterface;
+
 /**
  * Describes a logger instance
  *
@@ -37,89 +39,4 @@ namespace YoutubeDownloader\Logger;
  * See https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md
  * for the full interface specification.
  */
-interface Logger
-{
-    /**
-     * System is unusable.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function emergency($message, array $context = []);
-
-    /**
-     * Action must be taken immediately.
-     *
-     * Example: Entire website down, database unavailable, etc. This should
-     * trigger the SMS alerts and wake you up.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function alert($message, array $context = []);
-
-    /**
-     * Critical conditions.
-     *
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function critical($message, array $context = []);
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function error($message, array $context = []);
-
-    /**
-     * Exceptional occurrences that are not errors.
-     *
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things
-     * that are not necessarily wrong.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function warning($message, array $context = []);
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function notice($message, array $context = []);
-
-    /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function info($message, array $context = []);
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string $message
-     * @param array  $context
-     */
-    public function debug($message, array $context = []);
-
-    /**
-     * Logs with an arbitrary level.
-     *
-     * @param mixed  $level
-     * @param string $message
-     * @param array  $context
-     */
-    public function log($level, $message, array $context = []);
-}
+interface Logger extends LoggerInterface {}
